073acd0b2628a9e709d8af7a39445f81879160f7
Vandyck <vandycklai@gmail.com>
2024-09-08 12:45:35 +0800

    Upload completed code

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_api/amplify_api.dart';
+// import 'package:my_amplify_app/graphql_api_view.dart';
+import 'package:my_amplify_app/models/ModelProvider.dart';
+import 'package:my_amplify_app/pages/todo_screen.dart';
+
+import 'amplify_outputs.dart';
+
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _configureAmplify();
+    runApp(const MyApp());
+  } on AmplifyException catch (e) {
+    runApp(Text("Error configuring Amplify: ${e.message}"));
+  }
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.addPlugins(
+      [
+        AmplifyAuthCognito(),
+        AmplifyAPI(
+          options: APIPluginOptions(
+            modelProvider: ModelProvider.instance,
+          ),
+        ),
+      ],
+    );
+    await Amplify.configure(amplifyConfig);
+    safePrint('Successfully configured');
+  } on Exception catch (e) {
+    safePrint('Error configuring Amplify: $e');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        home: const Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SignOutButton(),
+                Expanded(child: TodoScreen()),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
